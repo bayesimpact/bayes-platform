8b35c3881d2fdaba776c5c1202f5851c3653985d
Alexis Jamet <alexis.jamet@gmail.com>
2026-06-04 15:13:48 +0200

    fix: remove drop index in last mig

--- apps/api/src/migrations/1780429003248-index-evaluation-extraction-run-record-by-run-and-status.ts
+++ apps/api/src/migrations/1780429003248-index-evaluation-extraction-run-record-by-run-and-status.ts
@@ -6,14 +6,12 @@ export class IndexEvaluationExtractionRunRecordByRunAndStatus1780429003248
   name = "IndexEvaluationExtractionRunRecordByRunAndStatus1780429003248"
 
   public async up(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."IDX_c17f379c092804abade46043b7"`)
     await queryRunner.query(
       `CREATE INDEX "IDX_e58b9952ddea7ec269333a2778" ON "evaluation_extraction_run_record" ("organization_id", "project_id", "evaluation_extraction_run_id", "status") `,
     )
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.query(`DROP INDEX "public"."IDX_e58b9952ddea7ec269333a2778"`)
     await queryRunner.query(
       `CREATE INDEX "IDX_c17f379c092804abade46043b7" ON "evaluation_extraction_run_record" ("organization_id", "project_id") `,
     )
