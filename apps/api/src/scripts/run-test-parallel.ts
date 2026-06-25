731917f46af524f206fcc41563b17d42f8eb3ccc
Jérémie Doucy <jdoucy@gmail.com>
2026-06-25 11:37:04 +0200

    ci: check parallel workers

--- apps/api/src/scripts/run-test-parallel.ts
+++ apps/api/src/scripts/run-test-parallel.ts
@@ -106,6 +106,7 @@ async function main(): Promise<void> {
           "--experimental-vm-modules",
           "../../node_modules/jest/bin/jest.js",
           "--colors",
+          "--verbose",
           "--forceExit",
           `--maxWorkers=${resolvedWorkerCount}`,
           ...jestAdditionalArguments,
