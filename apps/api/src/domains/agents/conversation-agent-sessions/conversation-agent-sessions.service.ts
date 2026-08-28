9e09ed4c5494b4856adade2c6ae334ade71204cd
Augustin <augustin@MacBook-Pro-de-Augustin.local>
2026-08-28 09:18:36 +0200

    Revert "chore: inject AgentSubAgentsService into ConversationAgentSessionsService"
    This reverts commit 49ff4e820e00d7619b6a4b7901ca1f26a61226be.

--- apps/api/src/domains/agents/conversation-agent-sessions/conversation-agent-sessions.service.ts
+++ apps/api/src/domains/agents/conversation-agent-sessions/conversation-agent-sessions.service.ts
@@ -34,8 +34,6 @@ export class ConversationAgentSessionsService {
     agentSettingsRepository: Repository<AgentSettings>,
     @InjectRepository(ConversationAgentSessionCategory)
     conversationAgentSessionCategoryRepository: Repository<ConversationAgentSessionCategory>,
-    @Inject(AgentSubAgentsService)
-    private readonly agentSubAgentsService: AgentSubAgentsService,
   ) {
     this.conversationAgentSessionConnectRepository = new ConnectRepository(
       conversationAgentSessionRepository,
